e04bcdfa837900f06b642632b1440413e344398d
Jeroen van der Heijden <jeroen@cesbit.com>
2026-06-11 12:26:34 +0200

    comment

--- app/version.go
+++ app/version.go
@@ -1,3 +1,3 @@
 package app
 
-const Version = "1.0.2" // And update package[-lock].json, README.md, about modal (24x)
+const Version = "1.0.2" // + package[-lock].json, README.md, about modal (24x)
